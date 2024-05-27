4b2cf9c2b882afe40d1f525fbf871d38fcded853
Njagi Githaiga <njagigithaiga@gmail.com>
2024-05-27 06:09:24 +0300

    Add list comprehension expressions to perform for loops

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+# Question 1
 def return_evens(num_list):
     pass
+    evens_list = [n for n in num_list if n % 2 == 0]
+    return evens_list
 
+return_evens([0, 1, 3, 5, 7, 8, 9])
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
+
+# Question 2
 def make_exclamation(sentence_list):
     pass
+    exclaimed_sentences = [n + "!" for n in sentence_list]
+    return exclaimed_sentences
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
